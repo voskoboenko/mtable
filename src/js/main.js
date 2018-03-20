bc7153a205cd5f2fd4ae4f68c32e91209e1f4556
Andrey Voskoboenko <andreyhtml1997@gmail.com>
2018-03-20 12:58:37 +0200

    clear

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.js
+//= ../../bower_components/jquery-mask-plugin/dist/jquery.mask.min.js
+//= ../../bower_components/owl.carousel/dist/owl.carousel.min.js
+//= ../../bower_components/round-slider/dist/roundslider.min.js
+//= ../../bower_components/wow/dist/wow.min.js
+
+new WOW().init();
